cf464256a14ce02f3dab20763b09bedc74bd62bc
Achintya Jha <achntj@gmail.com>
2025-02-07 09:12:52 -0700

    design: top bar (slstatus)

--- suckless/dwm/config.h
+++ suckless/dwm/config.h
@@ -6,7 +6,7 @@ static const unsigned int borderpx  = 2;        /* border pixel of windows */
 static const unsigned int gappx     = 10;        /* gaps between windows */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
-static const int topbar             = 0;        /* 0 means bottom bar */
+static const int topbar             = 1;        /* 0 means bottom bar */
 static const char *fonts[]          = { "FiraCode Nerd Font Mono:size=12" };
 static const char dmenufont[]       = "FiraCode Nerd Font Mono:size=12";
 // background color
